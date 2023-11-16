ccb8a37703cc2be60f90ad5ac910cab1e63613c2
Olivier Mocquais <omocquais@vmware.com>
2023-11-16 10:24:09 +0100

    Add integration test with @SpringBootTest

--- src/test/java/com/example/demopocreactive/CustomerControllerTest.java
+++ src/test/java/com/example/demopocreactive/CustomerControllerTest.java
@@ -6,22 +6,29 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
-@ExtendWith(SpringExtension.class)
-@WebFluxTest(CustomerController.class)
+import static org.assertj.core.api.Assertions.assertThat;
+
+//@ExtendWith(SpringExtension.class)
+//@WebFluxTest(CustomerController.class)
+@SpringBootTest
 class CustomerControllerTest {
 
-    @Autowired
-    private WebTestClient webClient;
+//    @Autowired
+//    private WebTestClient webClient;
 
     @Test
     void create() throws JsonProcessingException {
 
         CustomerDTO customerDTO = new CustomerDTO("John", "Smith");
         String json = new ObjectMapper().writeValueAsString(customerDTO);
+
+        WebTestClient webClient = WebTestClient.bindToController(new CustomerController())
+                .build();
 
         webClient
                 .post()
@@ -31,10 +38,11 @@ class CustomerControllerTest {
                 .exchange()
                 .expectStatus()
                 .isOk()
-                .expectBody()
-                .jsonPath("$.uuid").isNotEmpty()
-                .jsonPath("$.firstName").isEqualTo(customerDTO.firstName())
-                .jsonPath("$.name").isEqualTo(customerDTO.name());
+                .expectBody(CustomerResponseDTO.class).value(customerResponseDTO -> {
+                    assertThat(customerResponseDTO.uuid()).isNotNull();
+                    assertThat(customerResponseDTO.firstName()).isNotNull().isEqualTo(customerDTO.firstName());
+                    assertThat(customerResponseDTO.name()).isNotNull().isEqualTo(customerDTO.name());
+                });
 
     }
 }
